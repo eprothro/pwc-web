651cf052d5fb26ab0b42d06a558e35e1a5a97750
Evan Prothro <evan.prothro@gmail.com>
2015-04-10 11:16:37 -0500

    record not found => 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,15 @@ class ApplicationController < ActionController::Base
 
   unless Rails.application.config.consider_all_requests_local
     rescue_from Exception, with: :generic_error
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   end
 
   private
 
   def generic_error(exception)
     return render "pages/error", status: 500
+  end
+  def record_not_found(exception)
+    return render "pages/not_found", status: 404
   end
 end
